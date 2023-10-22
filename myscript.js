2039046e0371f2d489ec9c2a8ce85d8025d7d2d4
Maheswariparamasivan <146523568+Maheswariparamasivan@users.noreply.github.com>
2023-10-22 21:25:22 +0530

    pro2

---
+++ myscript.js
@@ -0,0 +1,10 @@
+function click(){
+    confirm('Do you want to view my badge?');
+}
+var menu=document.getElementById('menu');
+function closemenu(){
+    menu.style.top="-100vh";
+}
+function openmenu(){
+  menu.style.top="17%";
+}
